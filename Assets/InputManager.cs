77ab2b611753fabc35f4467c7b7527fcbe70b48f
zDe4N <103891170+zDe4N@users.noreply.github.com>
2023-02-22 15:38:53 +0100

    InputManager
    Riconoscimento Input Joystick

---
+++ Assets/InputManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.XR;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private XRNode xRNode= XRNode.LeftHand;
+    private List<InputDevice> devices = new List<InputDevice>();
+    private InputDevice device;
+    void Start()
+    {
+        
+
+    }
+
+    void GetDevice(){
+        InputDevices.GetDevicesAtXRNode(xRNode,devices);
+        device = devices.FirstOrDefault(); 
+    }
+    void OnEnable(){
+
+        if(!device.isValid){
+            GetDevice();
+        }
+
+    }
+
+        int cont=0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!device.isValid){
+            GetDevice();
+        }
+        /*List<InputFeatureUsage> features = new List<InputFeatureUsage>();
+        device.TryGetFeatureUsages(features); 
+        if(cont>features.Count){
+        return;
+        }else{
+        foreach(var feature in features){
+            if(feature.type== typeof(bool)){
+        Debug.Log($"feature{feature.name} type {feature.type}");
+            }
+        cont++;
+        
+        } Logg per riconoscere input*/ 
+         input(); 
+        }
+ 
+    public bool triggerButtonAction= false;
+    public bool gripButton= false;
+     void input(){
+            
+                
+
+        
+        if(device.TryGetFeatureValue(CommonUsages.triggerButton,out triggerButtonAction)&& triggerButtonAction){
+
+                    Debug.Log($"Trigger button activated {triggerButtonAction}");
+
+        }
+       
+        if(device.TryGetFeatureValue(CommonUsages.gripButton,out gripButton)&& gripButton){
+
+                    Debug.Log($"Trigger button activated {gripButton}");
+
+        }
+        
+     }
+     
+
+    }
+        
+
+
+
+        
+
+
