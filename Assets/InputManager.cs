61cfd6c5619b2662737b2576513a0f8a29fac985
zDe4N <103891170+zDe4N@users.noreply.github.com>
2023-02-28 09:19:31 +0100

    Aggiunto ObjectGrabber
    Serve per riconoscere quando l'oggetto è preso o rilasciato con i metodi OnRelease / OnGrab e fix di inputManager

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -68,15 +68,7 @@ public class InputManager: MonoBehaviour {
 		}
 
 	}
-    public bool isHoldItem;
-	public void isholdItem(){
-
-		isHoldItem=true;
-	}
-
-	public void notHoldItem(){
-		isHoldItem = false;
-	}
+   
 
 
 
