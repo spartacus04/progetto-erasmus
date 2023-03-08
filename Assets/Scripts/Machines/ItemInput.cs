698701e7ec05d477dee4968a51d4db14b94275e9
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2023-03-08 12:59:46 +0100

    aggingo roba

---
+++ Assets/Scripts/Machines/ItemInput.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using static Glob;
+using TMPro;
+
+public class ItemInput : Machine
+{
+	public Item item;
+
+	public int count = 0; 
+
+	public override bool allowFluids => false;
+
+	private SpriteRenderer spriteRenderer;
+
+	private TextMeshPro text;
+
+	public override void clearContents() { }
+
+	public void Start() {
+		inventory = new Item[1];
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		spriteRenderer.sprite = item.icon;
+
+		text = GetComponentInChildren<TextMeshPro>();
+	}
+
+	public override void inventoryOperation(InteractionType type, ref Item current)
+	{
+		if(type == InteractionType.PUSH) { 
+			if(current != null && current.name == item.name) {
+				count += current.amount;
+				current = null;
+			}
+		}
+	}
+
+	private void Update() {
+		text.text = $"{count}/{item.maxStackSize}";
+	}
+
+	public override void onTick() { }
+}
